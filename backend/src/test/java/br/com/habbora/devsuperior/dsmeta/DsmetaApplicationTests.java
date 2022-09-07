3ee5560b1a7d4a278de70f3a4c114a35d046bd66
Alisson Queiroz <alisson.queirois@gmail.com>
2022-09-07 15:31:26 -0300

    Criando Projeto

---
+++ backend/src/test/java/br/com/habbora/devsuperior/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.habbora.devsuperior.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
